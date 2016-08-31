5f820573d35926dda4c2abe2bdc0a276cd0e5d8e
pengyn <393507488@qq.com>
2016-08-31 13:03:39 +0800

    第一次尝试提交

--- app/src/main/java/com/pyn/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/pyn/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pyn.coolweather;
+package com.pyn.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pyn.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
