48f8dbffedf5d3ba56e6e100f69f60e530e805a6
pengyn <393507488@qq.com>
2017-01-04 17:55:23 +0800

    logo

--- app/src/main/java/com/pyn/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/pyn/coolweather/activity/WeatherActivity.java
@@ -265,7 +265,7 @@ public class WeatherActivity extends AppCompatActivity {
             weatherLayout.setVisibility(View.VISIBLE);
 
             Intent intent = new Intent(this, AutoUpdateService.class);
-            startActivity(intent);
+            startService(intent);
 
         } else {
             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
